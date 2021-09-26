49c07e0848534acc7c8082c94aeaed4565d0e657
wooseok0727 <crash2@naver.com>
2021-09-27 02:05:42 +0900

    ssr

---
+++ ssr-recipe/src/pages/BluePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Blue from "../comps/Blue";
+
+const BluePage = () => {
+  return <Blue />;
+};
+
+export default BluePage;
---
+++ ssr-recipe/src/pages/RedPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Red from "../comps/Red";
+
+const RedPage = () => {
+  return <Red />;
+};
+
+export default RedPage;
